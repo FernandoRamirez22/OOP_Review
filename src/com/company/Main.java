c2aa982ab86f238879c37f955903e4a62f334c79
Fernando Ramirez <feramirez5725@eagle.fgcu.edu>
2019-09-04 18:33:38 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static <tesla> void main(String[] args) {
+Tesla myTesla = new Tesla();
+myTesla.setModel("575");
+
+
+Tesla yourTesla = new Tesla(){
+    yourTesla.setModel("s P100D")
+}
+    }
+}
---
+++ src/com/company/Tesla.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Tesla {
+    private String model;
+    private String vin;
+    private int batterySize;
+
+
+    public String getModel(){
+        return model;
+    }
+
+    private void setModel(String model){
+        this.model = model;
+
+
+        public void setModel(String m) {
+            model = m;
+        }
+    }
+}
